62bd7ce5952796a32b2699e575656368c75e920f
Will Flaherty <will@seatgeek.com>
2014-05-22 00:12:42 -0400

    Initial BobbyBonilla Commit

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+$(function () {
+    $('.maintitle').textillate();
+})
+
+$(function () {
+    $('.salaryowed').textillate({ initialDelay: 3500, inEffects: [ ], in: {delayScale: 7, reverse: true}});
+})
